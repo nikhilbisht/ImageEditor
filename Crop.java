8ddf7ecb3f12b83e3f8ea5d51662a208e55b47e6
nikhilbisht <nikhilbisht96@gmail.com>
2017-04-07 20:36:27 +0530

    Update Crop.java

--- Crop.java
+++ Crop.java
@@ -1,15 +1,17 @@
 import java.applet.*; 
-import java.awt.*; import java.awt.image.*;
+import java.awt.*;
+import java.awt.image.*;
  public class Crop extends Applet 
 { Image i, j; 
 public void init () 
 { MediaTracker mt = new MediaTracker (this);
- i = getImage(getDocumentBase(), "rosey.jpg"); mt.addImage (i, 0); 
+ i = getImage(getDocumentBase(), "rosey.jpg");
+ mt.addImage (i, 0); // adds an image to the list of images bring tracked by this mediatracker 
 try 
-{ mt.waitForAll(); 
-int width = i.getWidth(this); 
-int height = i. getHeight(this); 
-j = createImage (new FilteredImageSource(i.getSource(), new CropImageFilter(width/3, height/3, width/3, height/3))); 
+{ mt.waitForAll(); //start loading all images tracked by this mediatracker
+int width = i.getWidth(this); //get height of image
+int height = i. getHeight(this); //getwidth of image
+j = createImage (new FilteredImageSource(i.getSource(), new CropImageFilter(width/3, height/3, width/3, height/3))); //cropping the image
 } 
 catch (InterruptedException e) { e.printStackTrace(); } 
 } 
@@ -17,7 +19,7 @@ catch (InterruptedException e) { e.printStackTrace(); }
 public void paint (Graphics g) 
 { g.drawImage (i, 10, 10, this); // regular 
 
-if (j != null) { g.drawImage (j, 10, 90, this); // cropped } 
+if (j != null) { g.drawImage (j, 10, 90, this); //it will draw the cropped image } 
 }
 
  }
